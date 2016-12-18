b2d695aa75421b2505f452648afa4d34d958c9f5
agatan <agatan039@gmail.com>
2016-12-18 18:25:13 +0900

    Implement next method

--- syntax/src/scanner.rs
+++ syntax/src/scanner.rs
@@ -4,7 +4,7 @@ use position::File;
 
 #[derive(Debug, Clone)]
 pub struct Scanner<'a> {
-    file: &'a File<'a>,
+    file: File<'a>,
     src: &'a str,
     errors: &'a ErrorList<'a>,
 
@@ -17,8 +17,8 @@ pub struct Scanner<'a> {
 }
 
 impl<'a> Scanner<'a> {
-    pub fn new(file: &'a File<'a>, src: &'a str, errors: &'a ErrorList<'a>) -> Self {
-        Scanner{
+    pub fn new(file: File<'a>, src: &'a str, errors: &'a ErrorList<'a>) -> Self {
+        let mut sc = Scanner{
             file: file,
             src: src,
             errors: errors,
@@ -28,6 +28,38 @@ impl<'a> Scanner<'a> {
             read_offset: 0,
 
             error_count: 0,
+        };
+        sc.next();
+        sc
+    }
+
+    fn error(&mut self, offset: usize, msg: String) {
+        self.errors.add(self.file.position(self.file.pos(offset)), msg);
+        self.error_count += 1;
+    }
+
+    fn is_eof(&self) -> bool {
+        self.read_offset >= self.src.len()
+    }
+
+    fn next(&mut self) {
+        if self.is_eof() {
+            self.offset = self.src.len();
+            self.ch = 0 as char;
+        } else {
+            self.offset = self.read_offset;
+            if self.ch == '\n' {
+                self.file.add_line(self.offset);
+            }
+            self.ch = self.src[self.read_offset..].chars().next().unwrap();
+            let w = self.ch.len_utf8();
+            self.read_offset += w;
+        }
+    }
+
+    fn skip_whitespace(&mut self) {
+        while self.ch == ' ' || self.ch == '\t' || self.ch == '\n' || self.ch == '\r' || self.is_eof() {
+            self.next();
         }
     }
 }
@@ -50,6 +82,6 @@ mod tests {
         ];
         let file = File::new(Some("test"), input.len());
         let errors = ErrorList::new();
-        let sc = Scanner::new(&file, input, &errors);
+        let sc = Scanner::new(file, input, &errors);
     }
 }
