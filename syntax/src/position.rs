68506b6a80bec2aa75fd57fe77c0f353a2f32a88
agatan <agatan039@gmail.com>
2016-12-18 13:31:49 +0900

    Add Dummy position

--- syntax/src/position.rs
+++ syntax/src/position.rs
@@ -3,37 +3,38 @@ use std::fmt;
 /// `Position` represents a source position including file name, line number, column number and
 /// byte offset.
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
-pub struct Position<'a> {
-    pub filename: Option<&'a str>, // None if the source is stdin
-    pub line: isize, // starting at 1. if 0, the position is dummy.
-    pub column: isize, // byte offset from the head of the line, starting at 1
-    pub offset: isize, // byte offset from the head of the file, starting at 0
+pub enum Position<'a> {
+    Dummy,
+    FilePos {
+        filename: Option<&'a str>, // None if the source is stdin.
+        line: isize, // starting at 1. if 0, the position is dummy.
+        column: isize, // byte offset from the head of the line, starting at 1
+        offset: isize, // byte offset from the head of the file, starting at 0
+    },
 }
 
 impl<'a> Position<'a> {
     pub fn dummy() -> Self {
-        Position {
-            filename: None,
-            line: 0,
-            column: 0,
-            offset: 0,
-        }
+        Position::Dummy
     }
 
     pub fn is_valid(&self) -> bool {
-        self.line > 0 && self.column > 0
+        match *self {
+            Position::Dummy => false,
+            _ => true,
+        }
     }
 }
 
 impl<'a> fmt::Display for Position<'a> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if self.is_valid() {
-            match self.filename {
-                None => write!(f, "{}:{}", self.line, self.column),
-                Some(name) => write!(f, "{}:{}:{}", name, self.line, self.column),
+        match *self {
+            Position::Dummy => "-".fmt(f),
+            Position::FilePos { filename, line, column, .. } =>
+            match filename {
+                None => write!(f, "{}:{}", line, column),
+                Some(name) => write!(f, "{}:{}:{}", name, line, column),
             }
-        } else {
-            "-".fmt(f)
         }
     }
 }
