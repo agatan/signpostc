a13e9c61212851d52c846155c77d6fd6d371bd83
agatan <agatan039@gmail.com>
2016-12-18 12:16:11 +0900

    Add token module

---
+++ syntax/src/token.rs
@@ -0,0 +1,102 @@
+/// represents tokens of source code.
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
+pub struct Token<'a> {
+    pub kind: TokenKind,
+    pub literal: &'a str,
+}
+
+impl<'a> Token<'a> {
+    pub fn new(kind: TokenKind, literal: &'a str) -> Self {
+        Token {
+            kind: kind,
+            literal: literal,
+        }
+    }
+}
+
+/// Kind of tokens.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum TokenKind {
+    Error,
+
+    EOF,
+
+    Int,
+    True,
+    False,
+    Uident,
+    Ident,
+
+    Operator,
+
+    Eq,
+    Pipe,
+    Pipepipe, // to distinguish `|| { 1 }`
+
+    Arrow, // ->
+    FatArrow, // =>
+
+    // Delimiters
+    Comma,
+    Colon,
+    Semicolon,
+
+    Lparen,
+    Rparen,
+    Lbrace,
+    Rbrace,
+    Lbrack,
+    Rbrack,
+
+    // keywords
+    Def,
+    Let,
+    Struct,
+    Enum,
+    Interface,
+    Implement,
+
+    If,
+    Else,
+    Match,
+    Return,
+    For,
+    In,
+    Continue,
+    Break,
+}
+
+impl TokenKind {
+    pub fn lookup(s: &str) -> Self {
+        match s {
+            "true" => TokenKind::True,
+            "false" => TokenKind::False,
+            "def" => TokenKind::Def,
+            "let" => TokenKind::Let,
+            "struct" => TokenKind::Struct,
+            "enum" => TokenKind::Enum,
+            "interface" => TokenKind::Interface,
+            "implement" => TokenKind::Implement,
+            "if" => TokenKind::If,
+            "else" => TokenKind::Else,
+            "match" => TokenKind::Match,
+            "return" => TokenKind::Return,
+            "for" => TokenKind::For,
+            "in" => TokenKind::In,
+            "continue" => TokenKind::Continue,
+            "break" => TokenKind::Break,
+            _ => TokenKind::Ident,
+        }
+    }
+
+    pub fn lookup_operator(s: &str) -> Self {
+        match s {
+            "=" => TokenKind::Eq,
+            "|" => TokenKind::Pipe,
+            "||" => TokenKind::Pipepipe,
+            "->" => TokenKind::Arrow,
+            "=>" => TokenKind::FatArrow,
+            _ => TokenKind::Operator,
+        }
+    }
+}
