ed8bd4e3c5ec2d2971d38be143c9177cd5833e6b
agatan <agatan039@gmail.com>
2016-12-29 18:55:07 +0900

    Stub parse_program

--- syntax/src/ast.rs
+++ syntax/src/ast.rs
@@ -1,1 +1,9 @@
-pub type Program = ();
+#[derive(Debug, Clone)]
+pub struct Program {
+    pub decls: Vec<Decl>,
+}
+
+#[derive(Debug, Clone)]
+pub enum Decl {
+    Dummy
+}
--- syntax/src/parser.rs
+++ syntax/src/parser.rs
@@ -1,35 +1,48 @@
 use std::rc::Rc;
 use std::cell::RefCell;
 
+use token::{Token, TokenKind};
 use position::File;
 use scanner::Scanner;
 use errors::ErrorList;
-use ast;
+use ast::{Program, Decl};
 
 #[derive(Debug)]
 pub struct Parser<'a> {
     scanner: Scanner<'a>,
     errors: Rc<RefCell<ErrorList>>,
+
+    cur: Token,
 }
 
 impl<'a> Parser<'a> {
     pub fn new(file: File, src: &'a str) -> Self {
         let errors = Rc::new(RefCell::new(ErrorList::new()));
-        let sc = Scanner::new(file, src, errors.clone());
+        let mut sc = Scanner::new(file, src, errors.clone());
+        let token = sc.scan();
         Parser {
             scanner: sc,
             errors: errors,
+
+            cur: token,
         }
     }
 
-    pub fn parse_program(&mut self) -> ast::Program {
-        let _ = self.scanner.scan();
-        ()
+    pub fn parse_program(&mut self) -> Program {
+        let mut decls = Vec::new();
+        while self.cur.kind() != TokenKind::EOF {
+            decls.push(self.parse_decl());
+        }
+        Program { decls: decls }
     }
 
     pub fn into_errors(self) -> ErrorList {
         let errors = self.errors.clone();
         ::std::mem::drop(self);
         Rc::try_unwrap(errors).expect("errors ref count should be 1 here").into_inner()
+    }
+
+    pub fn parse_decl(&mut self) -> Decl {
+        Decl::Dummy
     }
 }
